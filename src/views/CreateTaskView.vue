b82dbbce80b52aac8a04d4d5c9803e10dade5101
Muhammad Kashif <mk.kashif.se@gmail.com>
2023-09-10 21:10:56 +0500

    responsive modified

--- src/views/CreateTaskView.vue
+++ src/views/CreateTaskView.vue
@@ -15,13 +15,13 @@ const estimatedTime = ref<string>('')
         <v-form>
           <v-container>
             <v-row>
-              <v-col cols="12" md="9">
+              <v-col cols="12" md="8" sm="8">
                 <VTextField v-model="title" :counter="10" label="Title" required hide-details> </VTextField>
                 <VTextarea class="mt-2" v-model="description" :counter="10" label="Description" hide-details required>
                 </VTextarea>
                 <v-file-input class="mt-2" accept="image/*" label="File input"></v-file-input>
               </v-col>
-              <v-col cols="12" md="3" class="flex justify-end">
+              <v-col cols="12" md="4" lg="4" class="flex justify-end">
                 <v-date-picker></v-date-picker>
               </v-col>
             </v-row>
